242545ae7453900a716ef03a71471a1f4f3112ef
Minh Tran <mtran@directtechnology.com>
2020-02-07 22:26:20 -0800

    Express Router

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const campsiteRouter = require('./routes/campsiteRouter');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -8,50 +9,13 @@ const port = 3000;
 const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
-
-app.all('/campsites', (req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-});
-
-app.get('/campsites', (req, res) => {
-    res.end('Will send all the campsites to you');
-});
-
-app.post('/campsites', (req, res) => {
-    res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
-});
-
-app.put('/campsites', (req, res) => {
-    res.statusCode = 403;
-    res.end('PUT operation not supported on /campsites');
-});
-
-app.delete('/campsites', (req, res) => {
-    res.end('Deleting all campsites');
-});
+// We specify the path for the router here, so we don't need to specify it in the router module itself
+app.use('/campsites', campsiteRouter);
 
 
-// CampsiteId
 
-app.get('/campsites/:campsiteId', (req, res) => {
-    res.end(`Will send details of the campsite: ${req.params.campsiteId} to you`);
-});
+// CampsiteId routing methods
 
-app.post('/campsites/:campsiteId', (req, res) => {
-    res.statusCode = 403;
-    res.end(`POST operation not supported on /campsite/${req.params.campsiteId}`);
-});
-
-app.put('/campsites/:campsiteId', (req, res) => {
-    res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
-    res.end(`Will update the campsite: ${req.body.name} with description: ${req.body.description}`);
-});
-
-app.delete('/campsites/:campsiteId', (req, res) => {
-    res.end(`Deleting campsite: ${req.params.campsiteId}`);
-});
 
 app.use(express.static(__dirname + '/public'));
 
